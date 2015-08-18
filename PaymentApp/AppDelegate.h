526fa812cf0647cdce91e1fcb311843e7284c515
Nikita Demidov <nikitademidov@Mac-mini-Nikita.local>
2015-08-18 23:10:57 +0300

    Initial Commit

---
+++ PaymentApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PaymentApp
+//
+//  Created by Nikita Demidov on 18.08.15.
+//  Copyright (c) 2015 Nikita Demidov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
